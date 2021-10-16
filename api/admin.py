96ec00dba9fc90d021b4c2fb58afc49ba25ede11
mojaeb <mojaebi@gmail.com>
2021-10-16 22:17:56 +0330

    refactor: change admin settings

--- api/admin.py
+++ api/admin.py
@@ -4,7 +4,7 @@ from .models import Product
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['slug', 'title', 'code']
+    fields = ['slug', 'title', 'code', 'user', 'price', 'discount']
 
 
 admin.site.register(Product, ProductAdmin)
--- api/models.py
+++ api/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-# from .. import user
+from django.conf import settings
 
 
 class Product(models.Model):
     slug = models.CharField(max_length=200, null=False)
     title = models.CharField(max_length=200)
     code = models.CharField(max_length=50)
-    # user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     price = models.IntegerField(null=True)
     discount = models.IntegerField(null=True)
-
 
     def __str__(self):
         return self.title
